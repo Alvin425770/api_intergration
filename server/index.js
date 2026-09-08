b67f2feb1b7490ff42046e7a2535940c743ba68f
Alvin425570 <mwanyumbaalvin@gmail.com>
2026-09-08 20:58:47 +0300

    express backend

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+const studentRoutes = require('./routes/students');
+app.use('/api/students', studentRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import StudentsTable from './StudentsTable';
+
+function App() {
+    return (
+        <div>
+            <h1>Students</h1>
+            <StudentsTable />
+        </div>
+    );
+}
+
+export default App;
